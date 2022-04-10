a8a4c3a02699e847afeb3e7a75cd1ffbf554862a
Dominik <depta.dominik@op.pl>
2022-04-10 14:28:29 +0200

    [BE] Fixed regex pattern for first & last name

--- Clinic/Areas/Identity/Data/Registrar.cs
+++ Clinic/Areas/Identity/Data/Registrar.cs
@@ -8,12 +8,12 @@ namespace Clinic.Areas.Identity.Data
     {
         [MaxLength(30, ErrorMessage = "The first name should have maximum 30 characters")]
         [MinLength(3, ErrorMessage = "The first name should have at least 3 characters.")]
-        [RegularExpression("^((?!^First Name$)[a-zA-Z '])+$", ErrorMessage = "First name must be properly formatted.")]
+        [RegularExpression(@"^[\p{L}\p{M}' \.\-]+$", ErrorMessage = "First name must be properly formatted.")]
         public string? FirstName { get; set; }
 
         [MaxLength(30, ErrorMessage = "The last name should have maximum 30 characters")]
         [MinLength(3, ErrorMessage = "The last name should have at least 3 characters.")]
-        [RegularExpression("^((?!^Last Name$)[a-zA-Z '])+$", ErrorMessage = "Last name must be properly formatted.")]
+        [RegularExpression(@"^[\p{L}\p{M}' \.\-]+$", ErrorMessage = "Last name must be properly formatted.")]
         public string? LastName { get; set; }
 
         public ICollection<Appointment>? Appointments { get; set; }
