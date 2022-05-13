6232969e7e839c91f5c3dd3a5c901e4b0b9f4176
Dominik <depta.dominik@op.pl>
2022-05-13 18:25:14 +0200

    Migration - DisableNullableNames (please do Update-Database), FE part is also fixed

--- Clinic/Areas/Identity/Data/LabManager.cs
+++ Clinic/Areas/Identity/Data/LabManager.cs
@@ -6,15 +6,17 @@ namespace Clinic.Areas.Identity.Data
 {
     public class LabManager
     {
+        [Required(ErrorMessage = "The first name is required")]
         [MaxLength(30, ErrorMessage = "The first name should have maximum 30 characters")]
         [MinLength(3, ErrorMessage = "The first name should have at least 3 characters.")]
         [RegularExpression(@"^[\p{L}\p{M}' \.\-]+$", ErrorMessage = "First name must be properly formatted.")]
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "The last name is required")]
         [MaxLength(30, ErrorMessage = "The last name should have maximum 30 characters")]
         [MinLength(3, ErrorMessage = "The last name should have at least 3 characters.")]
         [RegularExpression(@"^[\p{L}\p{M}' \.\-]+$", ErrorMessage = "Last name must be properly formatted.")]
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
 
         public ICollection<LaboratoryExamination>? LaboratoryExaminations { get; set; }
 
