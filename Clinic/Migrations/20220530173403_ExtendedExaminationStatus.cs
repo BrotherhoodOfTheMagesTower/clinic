139c2e8408d76088bb4edbd8a98a097a3cc03699
Kaktusowy500 <k.lewand500@gmail.com>
2022-05-30 20:23:33 +0200

    Seperate cancel status for technician and manager

---
+++ Clinic/Migrations/20220530173403_ExtendedExaminationStatus.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clinic.Migrations
+{
+    public partial class ExtendedExaminationStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
